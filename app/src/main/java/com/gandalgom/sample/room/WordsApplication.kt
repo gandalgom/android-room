811634b8218deedfadeacb52bce25ace90ece392
Max Yoo <devbear74@gmail.com>
2023-03-08 10:26:02 +0900

    Populate the database

--- app/src/main/java/com/gandalgom/sample/room/WordsApplication.kt
+++ app/src/main/java/com/gandalgom/sample/room/WordsApplication.kt
@@ -1,14 +1,19 @@
 package com.gandalgom.sample.room
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 import com.gandalgom.sample.room.repository.database.WordRoomDatabase
 import com.gandalgom.sample.room.repository.WordRepository
 
 class WordsApplication : Application() {
+    // No need to cancel this scope as it'll be torn down with the process
+    private val applicationScope = CoroutineScope(SupervisorJob())
+
     // Using by lazy so the database and the repository are only created when they're needed
     // rather than when the application starts
-    private val database by lazy { WordRoomDatabase.getDatabase(this) }
+    private val database by lazy { WordRoomDatabase.getDatabase(this, applicationScope) }
 
     val repository by lazy { WordRepository(database.wordDao()) }
 }

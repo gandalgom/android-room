011a4d4853b0d90fab4b580947776b2c47630393
Max Yoo <devbear74@gmail.com>
2023-03-08 11:26:01 +0900

    Connect with the data

--- app/src/main/java/com/gandalgom/sample/room/views/MainActivity.kt
+++ app/src/main/java/com/gandalgom/sample/room/views/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.gandalgom.sample.room.views
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 import com.gandalgom.sample.room.R
+import com.gandalgom.sample.room.WordsApplication
+import com.gandalgom.sample.room.repository.database.Word
+import com.gandalgom.sample.room.viewmodels.WordViewModel
+import com.gandalgom.sample.room.viewmodels.WordViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private val wordViewModel: WordViewModel by viewModels {
+        WordViewModelFactory((application as WordsApplication).repository)
+    }
+    private val activityResultLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                result.data?.getStringExtra(NewWordActivity.EXTRA_REPLY)?.let { reply ->
+                    val word = Word(reply)
+                    wordViewModel.insert(word)
+                }
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    R.string.empty_not_saved,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,5 +46,15 @@ class MainActivity : AppCompatActivity() {
         val adapter = WordListAdapter()
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        wordViewModel.allWords.observe(this) { words ->
+            words?.let { adapter.submitList(it) }
+        }
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, NewWordActivity::class.java)
+            activityResultLauncher.launch(intent)
+        }
     }
 }
